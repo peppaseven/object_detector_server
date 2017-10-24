f69d051d329614cca048caaf37f89ddc89996cb9
peter <peter.chuh@gmail.com>
2017-10-24 20:56:51 +0800

    [raspberry]:add support for raspberry platform

--- clarify.sh
+++ clarify.sh
@@ -1,3 +1,3 @@
-NUM_WORKER=2
-BIND_ADDR=0.0.0.0:30000
+NUM_WORKER=1
+BIND_ADDR=127.0.0.1:30000
 gunicorn -w $NUM_WORKER -b $BIND_ADDR -p gunicorn.pid object_classify_svr:app --log-level=debug> engine_classify.log &
